0b10ecbd264c6a1446ab5d38657915bd549a7c32
Natalie Ryder <>
2017-12-22 19:30:43 -0800

    connects to firebase

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,53 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCSPKhc6UfQEVqZrmPnf90Ekxxtwyk2Yak",
+authDomain: "train-time-70634.firebaseapp.com",
+databaseURL: "https://train-time-70634.firebaseio.com",
+projectId: "train-time-70634",
+storageBucket: "train-time-70634.appspot.com",
+messagingSenderId: "777562335142"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainRef = database.ref().child("trains");
+
+// on submit button click
 $("#trainSubmit").on("click", function() {
 	event.preventDefault();
-	var train = {};
-	train.name = $("#trainName").val();
-	train.destination = $("#destination").val();
-	train.ftt = $("#firstTrainTime").val();
-	train.frequency = $("#frequency").val();
-	addTrain(train);
+	// get the value from the inputs
+	var name = $("#trainName").val();
+	var destination = $("#destination").val();
+	var ftt = $("#firstTrainTime").val();
+	var frequency = $("#frequency").val();
+	//push a new object to "trains"
+	trainRef.push().set({
+		name: name,
+		destination: destination,
+		firstTrainTime: ftt,
+		frequency: frequency,
+	});
+		$("input").val("");
 })
-$("#firstTrainTime").val("20:10");
-$("#frequency").val("15");
+
 function addTrain(train) {
-	console.log(train);
 	// the difference betwenn first train and now
-	var diff = moment().diff(moment(train.ftt, 'HH:mm'),'minutes');
-
-	// time passed since first train minus the remainer
+	var diff = moment().diff(moment(train.firstTrainTime, 'HH:mm'),'minutes');
+	//minutes til next train
 	var mod = diff%train.frequency;
+	// time passed since first train minus the remainer from the frequency
 	var mult = diff - mod;
-	// add mult + one interval length
-	var nextTrain = moment(train.ftt, 'HH:mm').add(mult ,'minutes').add(train.frequency,'minutes').format('hh:mm a');
-
-
-	console.log(diff);
-	console.log(mod);
+	// add mult + one interval length to get next train
+	var lastTrain = moment(train.firstTrainTime, 'HH:mm').add(mult ,'minutes').format('hh:mm a');
+	console.log(lastTrain);
+	var nextTrain = (moment(lastTrain, 'hh:mm a')).add(train.frequency,'minutes').format('hh:mm a');
 	console.log(nextTrain);
+
+
+	// console.log(diff);
+	// console.log(mod);
+	// console.log(nextTrain);
 	
 	var trainTime = moment(train.ftt, 'HH:mm')._i; 
 	// console.log(trainTime);
@@ -39,11 +63,21 @@ function addTrain(train) {
 
 	$(row).append(name).append(destination).append(frequency).append(nextTrainCell).append(minutesAway).appendTo("tbody");
 
-	$("input").val("");
-
 }
+$(document).ready(function() {
+	database.ref("trains").on("child_added", function(childSnapshot) {
+		console.log(childSnapshot.val());
+		addTrain(childSnapshot.val());
+	});
+	// clock = setInterval(function() {
+	// 	$("tbody").empty();
+	// 	database.ref("trains").on("value", function(snapshot) {
+	// 		$.each(snapshot.val(), function(key, value) {
+	// 			addTrain(value);
+	// 		});
+	// 	});
+	// }, 6000);
+});
 	
-	var randomDate = "02/23/1999";
-    var randomFormat = "MM/DD/YYYY";
-    var convertedDate = moment(randomDate, randomFormat);
-      console.log("converted date:", convertedDate);
+
+
